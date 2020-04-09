271bd3688c3cfecebce274588ded5a74836fbc84
Jon <jongraf1@gmail.com>
2020-04-09 12:39:06 -0500

    added ability to create a custom policy and attach to an existing role

--- main.tf
+++ main.tf
@@ -1,11 +1,37 @@
 variable "commit" {}
+variable "s3_lambda_deploy_bucket" {
+  type = string
+  default = "jgraf-lambda-deploy"
+}
 
 provider "aws" {
   version = "~> 2.0"
-  region  = "us-east-2"
+  region = "us-east-2"
   shared_credentials_file = "/c/Users/g825714/.aws/"
 }
 
+//creates a custom policy document with multiple policies applied
+data "aws_iam_policy_document" "example_policy_doc" {
+  statement {
+    sid = "1"
+    actions = [
+      "s3:GetBucketLocation",
+      "s3:ListBucket",
+    ]
+    resources = [
+      "arn:aws:s3:::${var.s3_lambda_deploy_bucket}",
+    ]
+  }
+}
+
+//creates a policy with the document of existing policies applied
+resource "aws_iam_policy" "example_policy" {
+  name = "example_policy"
+  path = "/"
+  policy = data.aws_iam_policy_document.example_policy_doc.json
+}
+
+//creates a role with default lambda policies
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
@@ -26,6 +52,12 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+//attaches my custom policy to my basic lambda role
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.example_policy.arn
+}
+
 resource "aws_lambda_function" "test_lambda" {
   function_name = "test-function"
   role = aws_iam_role.iam_for_lambda.arn
@@ -33,16 +65,17 @@ resource "aws_lambda_function" "test_lambda" {
   runtime = "python3.7"
   s3_bucket = aws_s3_bucket.lambda_deploy.bucket
   s3_key = "lambda_test_${var.commit}.zip"
-  depends_on = [aws_s3_bucket.lambda_deploy]
+  depends_on = [
+    aws_s3_bucket.lambda_deploy]
 }
 
 resource "aws_s3_bucket" "lambda_deploy" {
-  bucket = "jgraf-lambda-deploy"
+  bucket = var.s3_lambda_deploy_bucket
   force_destroy = true
-  acl    = "private"
+  acl = "private"
 
   tags = {
-    Name        = "Lambda Deploy"
+    Name = "Lambda Deploy"
     Environment = "Dev"
   }
 }
