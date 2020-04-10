32739283f796cf8bf68235edce45b318787836a3
Jon <jongraf1@gmail.com>
2020-04-10 13:04:42 -0500

    wrapped the lambda in api gateway for external exposure

--- main.tf
+++ main.tf
@@ -85,3 +85,59 @@ resource "aws_s3_bucket" "lambda_deploy" {
     Environment = "Dev"
   }
 }
+
+
+resource "aws_api_gateway_rest_api" "lambda_api" {
+  name = "my-test-api"
+  description = "This is my API for demonstration purposes"
+}
+
+resource "aws_api_gateway_resource" "MyDemoResource" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  parent_id = aws_api_gateway_rest_api.lambda_api.root_resource_id
+  path_part = "mydemoresource"
+}
+
+resource "aws_api_gateway_method" "MyDemoMethod" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.MyDemoResource.id
+  http_method = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "MyDemoIntegration" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.MyDemoResource.id
+  http_method = aws_api_gateway_method.MyDemoMethod.http_method
+  integration_http_method = "POST"
+  type = "AWS"
+  uri = aws_lambda_function.test_lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id = "AllowExecutionFromAPIGateway"
+  action = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.test_lambda.function_name
+  principal = "apigateway.amazonaws.com"
+}
+
+resource "aws_api_gateway_method_response" "response_200" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.MyDemoResource.id
+  http_method = aws_api_gateway_method.MyDemoMethod.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "MyDemoIntegrationResponse" {
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  resource_id = aws_api_gateway_resource.MyDemoResource.id
+  http_method = aws_api_gateway_method.MyDemoMethod.http_method
+  status_code = aws_api_gateway_method_response.response_200.status_code
+}
+
+resource "aws_api_gateway_deployment" "MyDemoDeployment" {
+  depends_on = [aws_api_gateway_integration.MyDemoIntegration]
+
+  rest_api_id = aws_api_gateway_rest_api.lambda_api.id
+  stage_name  = "test"
+}
