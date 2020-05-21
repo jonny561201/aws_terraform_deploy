7c5ef43b026672a49ae47b3fde4c0dfe9abd870f
Jon <jongraf1@gmail.com>
2020-05-21 10:52:26 -0500

    updated the python code to connect to sqs and drop a message

--- lambda_test.py
+++ lambda_test.py
@@ -1,3 +1,14 @@
+from boto3 import Session
+
+REGION = 'us-east-2'
+QUEUE = 'jgraf-awesome-queue'
+
+
 def test_function(event, context):
-    print("Im working")
-    return "success"
+    aws_session = Session(region_name=REGION)
+    sqs = aws_session.resource('sqs')
+    sqs_queue = sqs.get_queue_by_name(QueueName=QUEUE)
+    print('SQS url:' + sqs_queue.url)
+
+    sqs_queue.send_message(MessageBody='hey Jude')
+    return 'success'
