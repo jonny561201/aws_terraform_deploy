d48f64ff7b303b7ca65605e1b6bbe8219382c1a3
Graf <Graf.Jon@principal.com>
2020-05-21 16:19:32 -0500

    updated deployment script to bundle dependencies with zip file

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,36 @@
+#!/bin/bash
 
 VERSION=1.0.0
+WHITE='\033[0m'
+GREEN='\033[0;32m'
+PRESENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ZIP_DIR=lambda_test_
+
 
 function zipFiles {
+  echo -e "${GREEN}Zipping up directory${WHITE}"
   powershell.exe -nologo -noprofile -command "& { Add-Type -A 'System.IO.Compression.FileSystem'; [IO.Compression.ZipFile]::CreateFromDirectory('lambda_test_${VERSION}', 'lambda_test_${VERSION}.zip'); }"
 }
 
 function createDirectories {
-  mkdir lambda_test_${VERSION}
+  echo -e "${GREEN}Creating zip directory and copying python files${WHITE}"
+  mkdir "${ZIP_DIR}${VERSION}"
   cp app.py lambda_test_${VERSION}
   cp -r src lambda_test_${VERSION}
 }
 
 function cleanupDirectory {
-    rm -rf lambda_test_${VERSION}
+  echo -e "${GREEN}Deleting original zip directory${WHITE}"
+  rm -rf lambda_test_${VERSION}
+}
+
+function installPythonDependencies {
+  echo -e "${GREEN}Installing Python files to directory${WHITE}"
+  pip install --target="${PRESENT_DIR}/${ZIP_DIR}${VERSION}" -Ur requirements.txt
+  popd
 }
 
 createDirectories
+installPythonDependencies
 zipFiles
 cleanupDirectory
