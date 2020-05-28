709fdf9af718cdd2cabaad1777ea5572623935bd
Graf <Graf.Jon@principal.com>
2020-05-28 14:52:42 -0500

    simplified python code for demo app

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from boto3 import Session
 
-from src.test import get_google_response
+from src.api_service import get_google_response
 
-REGION = 'us-east-2'
+REGION = 'us-east-1'
 QUEUE = 'jgraf-awesome-queue'
 
 
+#event.body
 def test_function(event, context):
     get_google_response()
-    aws_session = Session(region_name=REGION)
-    sqs = aws_session.resource('sqs')
-    sqs_queue = sqs.get_queue_by_name(QueueName=QUEUE)
-    print('SQS url:' + sqs_queue.url)
-
+    sqs_queue = __get_sqs_queue()
     sqs_queue.send_message(MessageBody='hey Jude')
     return 'success'
+
+
+def __get_sqs_queue():
+    aws_session = Session(region_name=REGION)
+    sqs = aws_session.resource('sqs')
+    return sqs.get_queue_by_name(QueueName=QUEUE)
--- src/api_service.py
+++ src/api_service.py
@@ -4,5 +4,4 @@ import requests
 def get_google_response():
     url = 'https://www.google.com'
     response = requests.get(url)
-    print('-----------Google Query----------')
-    print(response)
+    return response.content
