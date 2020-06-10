65fd08f300001ee236d497105b9bbf835bf3ef17
Jon Graf <jongraf1@gmail.com>
2020-06-10 11:34:48 -0500

    added method to call sqs and put a message on the queue

--- node/index.js
+++ node/index.js
@@ -1,9 +1,20 @@
 require('@babel/register');
+const AWS = require('aws-sdk');
 const { call_api } = require('./src/api_service');
 
 exports.handler = async function(event) {
-    await call_api('http://www.google.com')
+    await call_api('http://www.google.com');
+    putMessageOnQueue('Hey Jude');
     return 'success'
-}
+};
 
-exports.handler({});
+
+const putMessageOnQueue = (message) => {
+    const sqs = new AWS.SQS({region : 'us-east-1'});
+    const sqsQueue = sqs.getQueueUrl({"QueueName": process.env.AWS_QUEUE}).promise();
+    const params = {
+        MessageBody: message,
+        QueueUrl: sqsQueue
+    };
+    sqs.sendMessage(params, () => console.log('Message put on queue'));
+};
--- node/src/api_service.js
+++ node/src/api_service.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch';
 
 export const call_api = async (url) => {
-    var requestOptions = {
+    const requestOptions = {
         method: "GET"
-    }
+    };
     const response = await fetch(url, requestOptions);
     console.log('api call response: ' + response.status);
     return response.status;
