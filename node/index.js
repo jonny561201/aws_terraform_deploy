2c000e665b9df6d51f7fb6ada693e21e0919a27c
Jon Graf <jongraf1@gmail.com>
2020-06-11 09:21:39 -0500

    WIP: added method to make sqs call to dump message

--- node/index.js
+++ node/index.js
@@ -4,17 +4,19 @@ const { call_api } = require('./src/api_service');
 
 exports.handler = async function(event, context) {
     await call_api('http://www.google.com');
-    putMessageOnQueue('Hey Jude');
+    await putMessageOnQueue('Hey Jude');
     return 'success'
 };
 
 
-const putMessageOnQueue = (message) => {
-    const sqs = new AWS.SQS({region : 'us-east-1'});
-    const sqsQueue = sqs.getQueueUrl({"QueueName": process.env.AWS_QUEUE}).promise();
+const putMessageOnQueue = async (message) => {
+    const sqs = new AWS.SQS({region : process.env.AWS_REGION});
+    const sqsQueue = sqs.getQueueUrl({"QueueName": process.env.AWS_QUEUE}).promise().then(data => {
+        console.log('QUEUE Url:', data);
+    });
     const params = {
         MessageBody: message,
         QueueUrl: sqsQueue
     };
-    sqs.sendMessage(params, () => console.log('Message put on queue'));
+    await sqs.sendMessage(params, () => console.log('Message put on queue'));
 };
