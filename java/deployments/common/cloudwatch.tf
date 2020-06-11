ead175f322dab9204838ed1c67766f0eee97a9f1
Jon Graf <jongraf1@gmail.com>
2020-06-11 09:19:00 -0500

    fixed issue where cloudwatch logs were broken

--- java/deployments/common/cloudwatch.tf
+++ java/deployments/common/cloudwatch.tf
@@ -1,5 +1,5 @@
 //create lambda cloudwatch logs
 resource "aws_cloudwatch_log_group" "JavaLambdaCloudWatchGroup" {
-  name = "/aws/lambda/${aws_lambda_function.JavaLambda.function_name}-${var.demo_type}"
+  name = "/aws/lambda/${aws_lambda_function.JavaLambda.function_name}"
   retention_in_days = 7
 }
--- node/deployments/common/cloudwatch.tf
+++ node/deployments/common/cloudwatch.tf
@@ -1,5 +1,5 @@
 //create lambda cloudwatch logs
 resource "aws_cloudwatch_log_group" "NodeLambdaCloudWatchGroup" {
-  name = "/aws/lambda/${aws_lambda_function.NodeLambda.function_name}-${var.demo_type}"
+  name = "/aws/lambda/${aws_lambda_function.NodeLambda.function_name}"
   retention_in_days = 7
 }
